2bd1010e9b73bb27fd92d721e3d2003b3c248557
GuiFornes <guillaume.fornes@bluenav.fr>
2024-02-29 12:34:08 +0100

    allow different frequencies for lidars

--- src/parse_cfg_file/parse_livox_lidar_cfg.cpp
+++ src/parse_cfg_file/parse_livox_lidar_cfg.cpp
@@ -92,7 +92,7 @@ bool LivoxLidarConfigParser::ParseUserConfigs(const rapidjson::Document &doc,
     } else {
       user_config.dual_emit_en = static_cast<uint8_t>(config["dual_emit_en"].GetInt());
     }
-	if (!config.HasMember("frame_id")) {
+    if (!config.HasMember("frame_id")) {
       user_config.frame_id = "livox_frame";
     } else {
       user_config.frame_id = static_cast<std::string>(config["frame_id"].GetString());
