fc5d70735da35bbeaf5876e3bed32895882dfea6
GuiFornes <guillaume.fornes@bluenav.fr>
2024-05-05 17:00:03 +0200

    adding launch_file for only the blueboat HAP

--- launch_ROS2/blueboat_lidar_config_hap.py
+++ launch_ROS2/blueboat_lidar_config_hap.py
@@ -8,44 +8,36 @@ import launch
 xfer_format   = 0    # 0-Pointcloud2(PointXYZRTL), 1-customized pointcloud format
 multi_topic   = 1    # 0-All LiDARs share the same topic, 1-One LiDAR one topic
 data_src      = 0    # 0-lidar, others-Invalid data src
-publish_freq  = 5.0  # freqency of publish, 5.0, 10.0, 20.0, 50.0, etc.
+hap_publish_freq  = 5.0 # freqency of publish, 5.0, 10.0, 20.0, 50.0, etc.
 output_type   = 0
 lvx_file_path = '/home/livox/livox_test.lvx'
 cmdline_bd_code = 'livox0000000001'
 
 cur_path = os.path.split(os.path.realpath(__file__))[0] + '/'
 cur_config_path = cur_path + '../config'
-user_config_path = os.path.join(cur_config_path, 'blueboat_lidar_config_all.json')
+hap_user_config_path = os.path.join(cur_config_path, 'blueboat_lidar_config_hap.json')
 ################### user configure parameters for ros2 end #####################
 
-livox_ros2_params = [
+hap_livox_ros2_params = [
     {"xfer_format": xfer_format},
     {"multi_topic": multi_topic},
     {"data_src": data_src},
-    {"publish_freq": publish_freq},
+    {"publish_freq": hap_publish_freq},
     {"output_data_type": output_type},
     {"lvx_file_path": lvx_file_path},
-    {"user_config_path": user_config_path},
+    {"user_config_path": hap_user_config_path},
     {"cmdline_input_bd_code": cmdline_bd_code}
 ]
 
 
 def generate_launch_description():
-    livox_driver = Node(
+    hap_livox_driver = Node(
         package='livox_ros_driver2',
         executable='livox_ros_driver2_node',
         name='livox_lidar_publisher',
         output='screen',
-        parameters=livox_ros2_params
+        parameters=hap_livox_ros2_params
         )
     return LaunchDescription([
-        livox_driver
-        # launch.actions.RegisterEventHandler(
-        #     event_handler=launch.event_handlers.OnProcessExit(
-        #         target_action=livox_rviz,
-        #         on_exit=[
-        #             launch.actions.EmitEvent(event=launch.events.Shutdown()),
-        #         ]
-        #     )
-        # )
+        hap_livox_driver,
     ])
