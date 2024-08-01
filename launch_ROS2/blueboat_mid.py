3a1a2049b253640602f78373575143c7f8d1475c
GuiFornes <guillaume.fornes@bluenav.fr>
2024-08-01 17:05:54 +0200

    5Hz freq lidars

--- launch_ROS2/blueboat_mid.py
+++ launch_ROS2/blueboat_mid.py
@@ -8,7 +8,7 @@ import launch
 xfer_format   = 0    # 0-Pointcloud2(PointXYZRTL), 1-customized pointcloud format
 multi_topic   = 1    # 0-All LiDARs share the same topic, 1-One LiDAR one topic
 data_src      = 0    # 0-lidar, others-Invalid data src
-mid_publish_freq  = 10.0 # freqency of publish, 5.0, 10.0, 20.0, 50.0, etc.
+mid_publish_freq  = 5.0 # freqency of publish, 5.0, 10.0, 20.0, 50.0, etc.
 output_type   = 0
 lvx_file_path = '/home/livox/livox_test.lvx'
 cmdline_bd_code = 'livox0000000001'
